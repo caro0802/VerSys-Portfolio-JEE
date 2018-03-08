ed5c385b994503cecfa083b701631aba2a7ff1bb
Ännikän <nnikn@172.16.53.126>
2018-03-08 11:48:05 +0100

    AnzeigeBean

---
+++ jTodo/src/main/java/dhbwka/wwi/vertsys/javaee/jtodo/ejb/AnzeigeBean.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright © 2018 Dennis Schulmeister-Zimolong
+ * 
+ * E-Mail: dhbw@windows3.de
+ * Webseite: https://www.wpvs.de/
+ * 
+ * Dieser Quellcode ist lizenziert unter einer
+ * Creative Commons Namensnennung 4.0 International Lizenz.
+ */
+package dhbwka.wwi.vertsys.javaee.jtodo.ejb;
+
+import dhbwka.wwi.vertsys.javaee.jtodo.jpa.Anzeige;
+import javax.annotation.security.RolesAllowed;
+import javax.ejb.Stateless;
+
+@Stateless
+@RolesAllowed("minimarkt-app-user")
+public class AnzeigeBean extends EntityBean<Anzeige, Long>{
+    public AnzeigeBean(){
+        super(Anzeige.class);
+    }
+    
+}
